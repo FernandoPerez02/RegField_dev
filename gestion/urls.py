1a1d56d02c209831ade04ebbcd8b1b2fc9e85069
FernandoPerez02 <osorioperezluisfernando@gmail.com>
2024-08-07 20:52:18 -0500

    Descarga de pdf y excel en manejo de cafe

--- gestion/urls.py
+++ gestion/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('obtener_tipo/', views.obtener_tiporegistro, name='obtenertiporegistro'),
     path('gestioncafe/', views.gestioncafe, name='gestioncafe'),
     path('editarcafe/<id_cafe>', views.editar, name='editarcafe'),
-    path('eliminar/<id_cafe>', views.eliminar, name='delete')
+    path('eliminar/<id_cafe>', views.eliminar, name='delete'),
+    path('descargar_excel/', views.descargar_excel, name='descargar_excel'),
+    path('descargar_pdf/', views.descargar_pdf, name='descargar_pdf'),
+
 ]
