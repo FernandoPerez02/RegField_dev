dee6752df4eff2549a78b5e120eaae590f83c62d
FernandoPerez02 <osorioperezluisfernando@gmail.com>
2024-08-12 17:09:29 -0500

    Configuracion del servidor ngrock

--- regfield/settings.py
+++ regfield/settings.py
@@ -23,9 +23,11 @@ import os
 SECRET_KEY = os.environ.get('SECRET_KEY', default='fallback-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.ngrok.io']
+
+CSRF_TRUSTED_ORIGINS = ['https://*.ngrok.io']
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
